7d6d75b6281a0ef5864d66c66de7b6ec361766c5
Dennis Patzer <d.patzer@heimrich-hannot.de>
2018-01-18 16:23:07 +0100

    aded latest

--- src/Backend/ReaderConfig.php
+++ src/Backend/ReaderConfig.php
@@ -9,7 +9,24 @@
 namespace HeimrichHannot\ReaderBundle\Backend;
 
 use Contao\Backend;
+use Contao\BackendUser;
 
 class ReaderConfig extends Backend
 {
+    /**
+     * Return the edit header button.
+     *
+     * @param array  $row
+     * @param string $href
+     * @param string $label
+     * @param string $title
+     * @param string $icon
+     * @param string $attributes
+     *
+     * @return string
+     */
+    public function editHeader($row, $href, $label, $title, $icon, $attributes)
+    {
+        return BackendUser::getInstance()->canEditFieldsOf('tl_reader_config') ? '<a href="'.$this->addToUrl($href.'&amp;id='.$row['id']).'" title="'.StringUtil::specialchars($title).'"'.$attributes.'>'.Image::getHtml($icon, $label).'</a> ' : Image::getHtml(preg_replace('/\.svg$/i', '_.svg', $icon)).' ';
+    }
 }
