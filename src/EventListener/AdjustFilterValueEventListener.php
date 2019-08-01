dd91f90378a7a0a450d491add13413b6894c70aa
Dennis Patzer <d.patzer@heimrich-hannot.de>
2019-08-01 17:14:36 +0200

    changes for category and tags

--- src/EventListener/AdjustFilterValueEventListener.php
+++ src/EventListener/AdjustFilterValueEventListener.php
@@ -8,6 +8,7 @@
 
 namespace HeimrichHannot\ReaderBundle\EventListener;
 
+use Contao\Database;
 use Contao\System;
 use HeimrichHannot\FilterBundle\Event\AdjustFilterValueEvent;
 use HeimrichHannot\ReaderBundle\Backend\FilterConfigElement;
@@ -18,6 +19,8 @@ class AdjustFilterValueEventListener
     public function onAdjustFilterValue(AdjustFilterValueEvent $event, string $eventName, EventDispatcherInterface $dispatcher)
     {
         $element = $event->getElement();
+        $dca = $event->getDca();
+        $table = $event->getConfig()->getFilter()['dataContainer'];
 
         if (FilterConfigElement::ALTERNATIVE_SOURCE_READER_BUNDLE_ENTITY !== $element->alternativeValueSource ||
             null === ($readerConfig = System::getContainer()->get('huh.utils.model')->findModelInstanceByPk(
@@ -31,9 +34,36 @@ class AdjustFilterValueEventListener
         }
 
         $instance = System::getContainer()->get('huh.reader.manager.reader')->retrieveItem();
+        $value = null;
 
-        $value = $instance->{$element->readerField};
-        $value = array_filter(!\is_array($value) ? explode(',', $value) : $value);
+        if ($dca['eval']['isCategoryField']) {
+            $value = [];
+
+            $associations = System::getContainer()->get('huh.categories.manager')->findAssociationsByParentTableAndEntityAndField(
+                $table, $instance->getRawValue('id'), $element->field
+            );
+
+            if (null !== $associations) {
+                $value = $associations->fetchEach('category');
+            }
+        } elseif ('cfgTags' === $dca['inputType']) {
+            $value = [];
+            $relationTable = $dca['relation']['relationTable'];
+            $idName = str_replace('tl_', '', $table).'_id';
+
+            $associations = Database::getInstance()->prepare(
+                "SELECT cfg_tag_id FROM $relationTable WHERE $idName = ?"
+            )->execute($instance->getRawValue('id'));
+
+            if ($associations->numRows > 0) {
+                while ($associations->next()) {
+                    $value[] = $associations->cfg_tag_id;
+                }
+            }
+        } else {
+            $value = $instance->{$element->readerField};
+            $value = array_filter(!\is_array($value) ? explode(',', $value) : $value);
+        }
 
         $event->setValue($value);
     }
