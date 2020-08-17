f9676a9cb419bcab3d54f7a989b98a3f371b634a
Thomas Körner <t.koerner@heimrich-hannot.de>
2020-08-17 15:33:17 +0200

    removed hook from config

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,20 +1,16 @@
 <?php
 
-/**
- * Hooks
- */
-$GLOBALS['TL_HOOKS']['parseFrontendTemplate'][] = [\HeimrichHannot\ReaderBundle\EventListener\ParseFrontendTemplateListener::class, '__invoke'];
-
-
-/**
- * Backend modules
+/*
+ * Copyright (c) 2020 Heimrich & Hannot GmbH
+ *
+ * @license LGPL-3.0-or-later
  */
 
 $GLOBALS['BE_MOD']['system']['reader_configs'] = [
-    'tables' => ['tl_reader_config']
+    'tables' => ['tl_reader_config'],
 ];
 
-/**
+/*
  * Frontend modules
  */
 array_insert(
@@ -25,14 +21,14 @@ array_insert(
     ]
 );
 
-/**
+/*
  * Permissions
  */
 $GLOBALS['TL_PERMISSIONS'][] = 'readerbundles';
 $GLOBALS['TL_PERMISSIONS'][] = 'readerbundlep';
 
-/**
+/*
  * Models
  */
-$GLOBALS['TL_MODELS']['tl_reader_config']         = \HeimrichHannot\ReaderBundle\Model\ReaderConfigModel::class;
+$GLOBALS['TL_MODELS']['tl_reader_config'] = \HeimrichHannot\ReaderBundle\Model\ReaderConfigModel::class;
 $GLOBALS['TL_MODELS']['tl_reader_config_element'] = \HeimrichHannot\ReaderBundle\Model\ReaderConfigElementModel::class;
